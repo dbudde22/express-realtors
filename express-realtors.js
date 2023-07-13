a5503acd7c84b2022673e2a6e299f0ea6e2e0de4
Dennis Budde <dennis@writerduet.com>
2023-07-13 17:49:25 -0500

    add css and more data

--- express-realtors.js
+++ express-realtors.js
@@ -3,12 +3,27 @@ const inputBar = document.querySelector('#inputBar');
 
 let name = document.querySelector("#name");
 let address = document.querySelector("#address");
+let price = document.querySelector("#price");
+let sqFt = document.querySelector("#sqFt");
+let bedrooms = document.querySelector("#bedrooms");
+let isPetFriendly = document.querySelector("#isPetFriendly");
+let isAffordable = document.querySelector("#isAffordable");
+
 
 input.addEventListener('submit', async (e) => {
     e.preventDefault();
     const selectedHomeType = document.querySelector('input.chooser:checked');
     const response = await axios.get(`http://localhost:3001/${selectedHomeType.value}/${inputBar.value}`);
     name.innerText = `${response.data.name}`;
-    address.innerText = `The address is ${response.data.address}`;
+    address.innerText = `Address: ${response.data.address}`;
+    price.innerText = `Price: ${response.data.price}`
+    sqFt.innerText = `${response.data.sqFt} square feet`;
+    bedrooms.innerText = `Bedrooms: ${response.data.bedrooms}`;
+    if (response.data.isPetFriendly) {
+        isPetFriendly.innerText = "Pet Friendly!"
+    };
+    if (response.data.isAffordable) {
+        isAffordable.innerText = "It's affordable imo!";
+    };
     inputBar.value = "";
 })
